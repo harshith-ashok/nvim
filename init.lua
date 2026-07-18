65c5a3486224c007a97f75fea6a3267710c72313
Harshith Ashok <harshith.7k10@gmail.com>
2026-07-18 14:40:40 +0530

    updates thingy

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
+-- =====================================================
 -- Leader
-
+-- =====================================================
 vim.g.mapleader = " "
 
--- Basic settings
+-- =====================================================
+-- Basic Settings
+-- =====================================================
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -15,250 +18,200 @@ vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus"
 
--- Plain black background
-vim.cmd("colorscheme default")
--- vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
 vim.cmd("syntax on")
 
+-- =====================================================
 -- Bootstrap lazy.nvim
+-- =====================================================
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",a
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath
-    })
+    vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath})
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-
+-- =====================================================
 -- Plugins
+-- =====================================================
 
-require("lazy").setup({
-    -- Icons
-    { "nvim-tree/nvim-web-devicons", opts = {} },
-    -- Buffer
-    {
-        "akinsho/bufferline.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-           require("bufferline").setup({})
-        end
-    },
-    -- Status Line
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
-
-          local aura = {
-
-              normal = {
-                  a = {
-                      fg = "#15141b",
-                      bg = "#a277ff",
-                      gui = "bold"
-                  },
-
-                  b = {
-                      fg = "#edecee",
-                      bg = "#21202e"
-                  },
-
-                  c = {
-                      fg = "#b6b2c7",
-                      bg = "#15141b"
-                  },
-              },
-
-              insert = {
-                  a = {
-                      fg = "#15141b",
-                      bg = "#61ffca",
-                      gui = "bold"
-                  },
-
-                  b = {
-                      fg = "#edecee",
-                      bg = "#21202e"
-                  },
-              },
-
-              visual = {
-                  a = {
-                      fg = "#15141b",
-                      bg = "#ffca85",
-                      gui = "bold"
-                  },
-
-                  b = {
-                      fg = "#edecee",
-                      bg = "#21202e"
-                  },
-              },
-
-              replace = {
-                  a = {
-                      fg = "#15141b",
-                      bg = "#ff6767",
-                      gui = "bold"
-                  },
-
-                  b = {
-                      fg = "#edecee",
-                      bg = "#21202e"
-                  },
-              },
-
-              command = {
-                  a = {
-                      fg = "#15141b",
-                      bg = "#82e2ff",
-                      gui = "bold"
-                  },
-
-                  b = {
-                      fg = "#edecee",
-                      bg = "#21202e"
-                  },
-              },
-
-              inactive = {
-                  a = {
-                      fg = "#6d6d6d",
-                      bg = "#15141b"
-                  },
-
-                  b = {
-                      fg = "#6d6d6d",
-                      bg = "#15141b"
-                  },
-
-                  c = {
-                      fg = "#6d6d6d",
-                      bg = "#15141b"
-                  },
-              },
-          }
-
-                require("lualine").setup({
-                    options = {
-                        theme = aura,
-
-                        section_separators = {
-                            left = "",
-                            right = "",
-                        },
-
-                        component_separators = {
-                            left = "│",
-                            right = "│",
-                        },
-
-                        globalstatus = true,
-                    },
-
-                    sections = {
-                        lualine_a = { "mode" },
-
-                        lualine_b = {
-                            "branch",
-                            "diff",
-                        },
-
-                        lualine_c = {
-                            {
-                                "filename",
-                                path = 1,
-                            }
-                        },
-
-                        lualine_x = {
-                            "encoding",
-                            "filetype",
-                        },
-
-                        lualine_y = {
-                            "progress",
-                        },
-
-                        lualine_z = {
-                            "location",
-                        },
-                    },
-                })
-            end
-    },
-    -- File explorer
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("nvim-tree").setup({
-                view = { width = 30 },
-            })
-        end
-    },
-
-    -- LSP
-    {
-        "neovim/nvim-lspconfig",
-    },
-
-    -- Aura Theme
-    {
-    "baliestri/aura-theme",
+require("lazy").setup({ -----------------------------------------------------
+-- Theme
+-----------------------------------------------------
+{
+    "Shatur/neovim-ayu",
     lazy = false,
     priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
+    config = function()
+
+        require("ayu").setup({
+            mirage = false,
+            terminal = true,
+            overrides = {}
+        })
+
+        vim.cmd("colorscheme ayu-light")
     end
-  },
-    -- Autocomplete
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        config = function()
-            local cmp = require("cmp")
+}, -----------------------------------------------------
+-- Icons
+-----------------------------------------------------
+{
+    "nvim-tree/nvim-web-devicons",
+    opts = {}
+}, -----------------------------------------------------
+-- Bufferline
+-----------------------------------------------------
+{
+    "akinsho/bufferline.nvim",
 
-            cmp.setup({
-                mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = {
-                    { name = "nvim_lsp" },
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+
+    config = function()
+
+        require("bufferline").setup({
+            options = {
+                diagnostics = "nvim_lsp",
+                separator_style = "slant"
+            }
+        })
+
+    end
+}, -----------------------------------------------------
+-- Lualine
+-----------------------------------------------------
+{
+    "nvim-lualine/lualine.nvim",
+
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+
+    config = function()
+
+        require("lualine").setup({
+
+            options = {
+
+                theme = "ayu",
+
+                globalstatus = true,
+
+                section_separators = {
+                    left = "",
+                    right = ""
+                },
+
+                component_separators = {
+                    left = "│",
+                    right = "│"
                 }
-            })
-        end
-    },
+            },
 
-})
+            sections = {
 
+                lualine_a = {"mode"},
 
--- LSP Setup (Neovim 0.11+)
+                lualine_b = {"branch", "diff"},
+
+                lualine_c = {{
+                    "filename",
+                    path = 1
+                }},
+
+                lualine_x = {"encoding", "filetype"},
+
+                lualine_y = {"progress"},
+
+                lualine_z = {"location"}
+            }
+
+        })
+
+    end
+}, -----------------------------------------------------
+-- Nvim Tree
+-----------------------------------------------------
+{
+    "nvim-tree/nvim-tree.lua",
+
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+
+    config = function()
+
+        require("nvim-tree").setup({
+
+            view = {
+                width = 30
+            }
+
+        })
+
+    end
+}, -----------------------------------------------------
+-- LSP
+-----------------------------------------------------
+{"neovim/nvim-lspconfig"}, -----------------------------------------------------
+-- Autocomplete
+-----------------------------------------------------
+{
+    "hrsh7th/nvim-cmp",
+
+    dependencies = {"hrsh7th/cmp-nvim-lsp"},
+
+    config = function()
+
+        local cmp = require("cmp")
+
+        cmp.setup({
+
+            mapping = cmp.mapping.preset.insert({
+
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                ["<CR>"] = cmp.mapping.confirm({
+                    select = true
+                })
+
+            }),
+
+            sources = {{
+                name = "nvim_lsp"
+            }}
+
+        })
+
+    end
+}})
+
+-- =====================================================
+-- LSP Setup
+-- =====================================================
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-vim.lsp.config("pyright", { capabilities = capabilities })
-vim.lsp.config("clangd", { capabilities = capabilities })
-vim.lsp.config("tsserver", { capabilities = capabilities })
-vim.lsp.config("lua_ls", { capabilities = capabilities })
+vim.lsp.config("pyright", {
+    capabilities = capabilities
+})
+
+vim.lsp.config("clangd", {
+    capabilities = capabilities
+})
+
+vim.lsp.config("tsserver", {
+    capabilities = capabilities
+})
+
+vim.lsp.config("lua_ls", {
+    capabilities = capabilities
+})
 
 vim.lsp.enable("pyright")
 vim.lsp.enable("clangd")
 vim.lsp.enable("tsserver")
 vim.lsp.enable("lua_ls")
 
-
+-- =====================================================
 -- Keymaps
+-- =====================================================
 
 local keymap = vim.keymap
 
@@ -271,23 +224,88 @@ keymap.set("n", "<C-l>", "<C-w>l")
 keymap.set("n", "<C-j>", "<C-w>j")
 keymap.set("n", "<C-k>", "<C-w>k")
 
--- Next / previous buffer
-vim.keymap.set("n", "<leader>.", ":bnext<CR>")
-vim.keymap.set("n", "<leader>,", ":bprevious<CR>")
-
--- Close buffer
-vim.keymap.set("n", "<leader>x", ":bdelete<CR>")
-
-
+keymap.set("n", "<leader>.", ":bnext<CR>")
+keymap.set("n", "<leader>,", ":bprevious<CR>")
+keymap.set("n", "<leader>x", ":bdelete<CR>")
+-- =====================================================
 -- LSP Keymaps
+-- =====================================================
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(e)
-        local opts = { buffer = e.buf }
+    callback = function(event)
+        local opts = {
+            buffer = event.buf
+        }
 
         keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
         keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
         keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    end,
+
+        keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({
+                async = true
+            })
+        end, opts)
+    end
+})
+
+-- =====================================================
+-- Diagnostics
+-- =====================================================
+
+vim.diagnostic.config({
+    virtual_text = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = "if_many"
+    }
+})
+
+-- =====================================================
+-- Floating Windows
+-- =====================================================
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded"
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded"
+})
+
+-- =====================================================
+-- General UI
+-- =====================================================
+
+vim.opt.pumblend = 10
+vim.opt.winblend = 10
+
+vim.cmd([[
+highlight NormalFloat guibg=NONE
+highlight FloatBorder guibg=NONE
+]])
+
+-- =====================================================
+-- Bufferline
+-- =====================================================
+
+vim.cmd([[
+highlight BufferLineFill guibg=NONE
+]])
+
+-- =====================================================
+-- NvimTree
+-- =====================================================
+
+vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", {
+    silent = true
 })
